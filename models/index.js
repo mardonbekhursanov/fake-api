042acf8b0559850afec9a73f25a0715cf97ee15f
mardonbekhursanov <mardonbekhursanov91@gmail.com>
2026-02-04 09:50:58 +0500

    SSL qo'shildi

--- models/index.js
+++ models/index.js
@@ -1,11 +1,19 @@
 const { Sequelize } = require("sequelize")
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    logging: false
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // ko‘p cloud DB lar uchun kerak
+    }
+  }
 })
+
 const db = {}
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize
+db.Sequelize = Sequelize
 db.Users = require("./user.model")(sequelize, Sequelize)
+
 module.exports = db
